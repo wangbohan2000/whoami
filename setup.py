8a121f46f9d8bd53e17961ecab36aaaa8159c423
Bohan Wang <wbhan_cn@qq.com>
2020-09-10 11:41:14 +0800

    change README

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     version="2.1.7",
     author="Bohan Wang",
     author_email="wbhan_cn@qq.com",
-    description="A convenient library to get our ip (real public ip or private ip).",
+    description=" ✔ A convenient library to get our ip (real public ip or private ip).",
     long_description=desc,
     long_description_content_type="text/markdown",
     url="https://github.com/wangbohan2000/whoami",
